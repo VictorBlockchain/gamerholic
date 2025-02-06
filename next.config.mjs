d02a5a002846b0a20fa7638b9bd25e7263495462
victorblockchain <93537143+VictorBlockchain@users.noreply.github.com>
2025-02-06 15:06:15 -0500

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
